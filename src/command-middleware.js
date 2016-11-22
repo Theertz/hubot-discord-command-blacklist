87e457c39c3d587efd71f22976d52efa414372d1
Matej Voboril <matej@voboril.org>
2016-11-22 17:42:54 -0600

    tweaks

--- src/command-middleware.js
+++ src/command-middleware.js
@@ -7,30 +7,32 @@
 
 module.exports = function (robot) {
   var owner = process.env.HUBOT_OWNER || '';
-  
   robot.listenerMiddleware(function (context, next, done) {
     if (context.response.message.text) {
-      var id = context.listener.options.id;
-      var room = robot.client.channels.find('id', context.response.envelope.room);
-      var hubot_user = context.response.envelope.user;
-      
-      var respondInChannel = robot.brain.get(`data.commandBlacklists${room.id}.replyInRoom`) || false;
-      var blacklist = robot.brain.get(`data.commandBlacklists${room.id}`) || [];
-      var override = robot.brain.get(`data.commandBlacklists${room.id}.override`) || false;
+      const id                = context.listener.options.id;
+      const room              = robot.client.channels.find('id', context.response.envelope.room);
+      const hubot_user        = context.response.envelope.user;
+      const respondInChannel  = robot.brain.get(`data.commandBlacklists${room.id}.replyInRoom`) || false;
+      const blacklist         = robot.brain.get(`data.commandBlacklists${room.id}`) || [];
+      const override          = robot.brain.get(`data.commandBlacklists${room.id}.override`) || false;
       robot.client.fetchUser(hubot_user.id)
         .then((user) =>{
-          var userHasPerm = room !== null ? room.permissionsFor(user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS") : user.id === owner;
-        
-          if (blacklist.indexOf(id) !== -1 && !(override && userHasPerm)) {
+          const userIsOwner = user.id === owner;
+          const userHasPerm = room !== null ? 
+          (room.type    === 'text') 
+          || room.type  === 'dm' 
+          || userIsOwner
+           : userIsOwner;
+          if ((blacklist.indexOf(id) !== -1 && !(override && userHasPerm)) && !userIsOwner) {
             if (respondInChannel) {
-              context.response.send(`Sorry, ${user.id} you aren't allowed to run that command in ${room}`);
+              context.response.send(`Sorry, ${user} you aren't allowed to run that command in ${room}`);
             }
             done();
           } else {
             next(done);
           }
       })
-      .catch((error) => console.error(`${error}, command-middleware.js: line 33`));
+      .catch((error) => console.error(`${error}, command-middleware.js: line ${error.lineNumber}`));
     } else {
       next(done);
     }
